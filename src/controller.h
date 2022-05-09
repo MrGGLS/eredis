d8dc47cb6db73d64875cf6a9fd7643d5ba90240b
mrzleo <mr.zleo10@gmail.com>
2022-05-09 23:35:03 +0800

    add(controller): define interface of controller

---
+++ src/controller.h
@@ -0,0 +1,15 @@
+#ifndef __CONTROLLER_H__
+#define __CONTROLLER_H__
+
+#include <string>
+
+class Controller {
+private:
+    /* Parser parser; */
+    /* DBOPS dbops; */
+public:
+    int run(std::string input);
+
+};
+
+#endif // __CONTROLLER_H__
